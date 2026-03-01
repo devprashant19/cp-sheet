af5f53278311ef1ecc75e2d175ba1b33050d2867
Dev Prashant <dev.prashant1901@gmail.com>
2026-03-01 20:20:10 +0530

    2180B

--- 2180B.cpp
+++ 2180B.cpp
@@ -1,12 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool lex(string a, string b){
-    for (long long i = 0; i < min(a.size(), b.size()); i++){
-        if (a[i] < b[i])return true;
-        else if (a[i] > b[i])return false;
-    }
-    return a.size() < b.size();
-}
 int main(){
     long long t;
     cin >> t;
@@ -17,8 +10,8 @@ int main(){
         for (long long i = 0; i < n; i++)cin >> a[i];
         string s = a[0];
         for(int i=1; i<n; i++){
-            if(lex(a[i], s))s.insert(0,a[i]);
-            else s.insert(s.size(),a[i]);
+            if(a[i]+s<s+a[i])s=a[i]+s;
+            else s=s+a[i];
         }
         cout << s << endl;
     }
