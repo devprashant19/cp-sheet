42e0778b63b4803c5f8de771328194a9b0dae924
devprashant19 <dev.prashant1901@gmail.com>
2026-03-26 14:45:52 +0530

    1807G2

--- 1807G2.cpp
+++ 1807G2.cpp
@@ -10,18 +10,21 @@ int main()
         vector<long long> a(n);
         for (int i=0; i<n; i++)cin>>a[i];
         sort(a.begin(), a.end());
-        if(a[0]!=1)cout<<"NO\n";
-        else{
-            long long sum=1;
-            for(int i=1;i<n;i++){
-                if(a[i]>sum) {
-                    cout<<"NO\n";
-                    break;
-                }
-                sum+=a[i];
-            }
-            cout<<"YES\n";
+        if(a[0]!=1){
+            cout<<"NO\n";
+            continue;
         }
+        bool flag=false;
+        long long sum=1;
+        for(int i=1;i<n;i++){
+            if(a[i]>sum) {
+                flag=true;
+                break;
+            }
+            sum+=a[i];
+        }
+        if(flag) cout<<"NO\n";
+        else cout<<"YES\n";
     }
     return 0;
 }
