d4c291048040c0f82286ba1714d253698737c70e
Dev Prashant <dev.prashant1901@gmail.com>
2026-04-04 16:11:49 +0530

    done

--- 1742D.cpp
+++ 1742D.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<long long>pairs[1001];
 int main()
 {
+    for (int i = 1; i <= 1000; ++i) {
+		for (int j = 1; j <= 1000; ++j) {
+			if (__gcd(i, j) == 1) {
+				pairs[i].push_back(j);
+			}
+		}
+	}
     int t;
     cin >> t;
     while (t--){
@@ -13,13 +21,14 @@ int main()
             cin >>x;
             idx[x]=i+1;
         }
-        int ans=-1;
+        long long ans=-1;
         for(int i=1;i<=1000;i++){
             if(idx[i]==0)continue;
-            for(int j)
-                ans=max(ans,idx[i]);
+            for(long long j:pairs[i]){
+                if(idx[j]!=0)ans=max(ans,idx[i]+idx[j]);
             }
         }
+        cout<<ans<<endl;
     }
     return 0;
 }
