366283aad3daca69586fce6c207a73a63f986ae5
Dev Prashant <dev.prashant1901@gmail.com>
2026-05-23 14:56:23 +0530

    done

---
+++ 1541B.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve() {
+    ll n;
+    cin>>n;
+    ll ans=0;
+    vector<pair<ll,ll>>v(n);
+    for(ll i=0;i<n;i++){
+        cin>>v[i].first;
+        v[i].second=i+1;
+    }
+    sort(v.begin(), v.end());
+    for(ll i=0;i<n;i++){
+        for(ll j=i+1;j<n;j++){
+            if(v[i].first*v[j].first>=2*n)break;
+            if(v[i].first*v[j].first==v[i].second+v[j].second)ans++;
+        }
+    }
+    cout<<ans<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
