e8291fa54bbed3354416310b775ab6d4f7ace45c
Dev Prashant <dev.prashant1901@gmail.com>
2026-08-07 21:39:37 +0530

    done

---
+++ 1498B.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve() {
+    ll n,w;
+    cin>>n>>w;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+    vector<int>temp(21,0);
+    for(int i=0;i<n;i++){
+        int val=log2(a[i]);
+        temp[val]++;
+    }
+    int height=0;
+    int boxes=0;
+    while(boxes<n){
+        int len=w;
+        for(int i=20;i>=0;i--){
+            while(temp[i]>0 && len>=(1<<i)){
+                len-=(1<<i);
+                temp[i]--;
+                boxes++;
+            }
+        }
+        height++;
+    }
+    cout<<height<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
